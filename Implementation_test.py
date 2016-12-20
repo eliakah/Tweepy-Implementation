70470b078ac67d6d77c54a4aa0a9cc2af1a9f6fb
eliakah <kakoue66@students.rowan.edu>
2016-12-20 08:52:35 -0500

    made it modular

--- Implementation_test.py
+++ Implementation_test.py
@@ -1,38 +1,63 @@
+# * Created by Eliakah kakou
+# Main.
+# This class runs all of the modules in the right sequential
+# order depending on the input
+
 import tweepy
 from time import sleep
 
-consumer_key="Z5dDoNOB1efZTIHsc3K39BTFa"
-consumer_secret="0OmTmSNH7whf7V06Z4LE3W4zXDAwxPKk9d948RLlQCBv7NgMyG"
-access_token="746388158-iY84sri3WGMULG9Hw3Q00ICurtA3pm8IMAho1gq7"
-access_token_secret="H6oTFv58fmrkmaluiNTuBKxgpRnFK08hgtMH5c79u6ZEG"
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret) #authorising your access, login
-auth.set_access_token(access_token, access_token_secret) #set access token
-auth.secure = True #make it secure = yes
-api = tweepy.API(auth) #allows us to use the API
-
-#print(str(api.get_user(screen_name = '@eliakah'))) # to chek if the screenname match and verify that we logged in
-
-##Let's begin the bot
+# main method
+def main():
+    bot = TwitterBot("Z5dDoNOB1efZTIHsc3K39BTFa", "0OmTmSNH7whf7V06Z4LE3W4zXDAwxPKk9d948RLlQCBv7NgMyG",
+                     "746388158-iY84sri3WGMULG9Hw3Q00ICurtA3pm8IMAho1gq7",
+                     "H6oTFv58fmrkmaluiNTuBKxgpRnFK08hgtMH5c79u6ZEG")
+    bot.getTweetsFromHash("finals")
 
 
-#this just fetches the most recent entries for #
-for tweet in tweepy.Cursor(api.search,q='#rowanuniversity', lang='en').items():
-    try:
-        print("Found tweet by: @"+ tweet.user.screen_name)
-        print("Found tweet by user_id: "+ str(tweet.user.id))
-        print("User Location: "+ str(tweet.user.location))
-        print("Saying: '" + tweet.text+"'\n")
+# this class makes call to the twitter api
+class TwitterBot:
+    # contsructor
+    def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret):
+        self.consumer_key = consumer_key  # "Z5dDoNOB1efZTIHsc3K39BTFa"
+        self.consumer_secret = consumer_secret  # "0OmTmSNH7whf7V06Z4LE3W4zXDAwxPKk9d948RLlQCBv7NgMyG"
+        self.access_token = access_token  # "746388158-iY84sri3WGMULG9Hw3Q00ICurtA3pm8IMAho1gq7"
+        self.access_token_secret = access_token_secret  # "H6oTFv58fmrkmaluiNTuBKxgpRnFK08hgtMH5c79u6ZEG"
 
-        #ids = []
-      #  for page in tweepy.Cursor(api.followers_ids, screen_name=tweet.user.screen_name).pages():
-           # ids.extend(page)
+    # this method prints the last 10 tweets from this hashtag
+    def getTweetsFromHash(self, hashtag):
 
-        #print("User has "+ str(len(ids)) + " followers \n")
+        hashtag = '#' + str(hashtag)
 
-    except tweepy.TweepError as e:
-        print (e.reason)
-        sleep(10)
-        continue
-    except StopIteration:
-        break
+        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)  # authorising your access, login
+        auth.set_access_token(self.access_token, self.access_token_secret)  # set access token
+        auth.secure = True  # make it secure = yes
+        api = tweepy.API(auth)  # allows us to use the API
+
+        print(hashtag)
+        # this just fetches the most recent entries for #
+
+        count = 0
+
+        for tweet in tweepy.Cursor(api.search, q=hashtag, lang='en').items():
+            if count == 10:
+                break
+            try:
+                print("Found tweet by: @" + tweet.user.screen_name)
+                print("Found tweet by user_id: " + str(tweet.user.id))
+                print("User Location: " + str(tweet.user.location))
+                print("Saying: '" + tweet.text + "'\n")
+                print("Saying: '" + tweet.date + "'\n")
+
+
+            except tweepy.TweepError as e:
+                print(e.reason)
+                sleep(10)
+                continue
+            except StopIteration:
+                break
+
+            count += 1
+
+
+main()  # main method call
